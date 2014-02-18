d6a2e1b5f2e9b3b744815476c1f8b17d1808fb09
ivo- <ivailo.nikolaev.hristov@gmail.com>
2014-02-18 21:49:49 +0200

    Add .lein

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-try "0.3.2"]
+                  [lein-exec "0.3.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-ancient "0.4.4"]
+                  [lein-bikeshed "0.1.3"]]}}
