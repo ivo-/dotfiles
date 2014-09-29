aa630353be9a35c4d5f83b95ecbaced2d77fd6d8
ivo- <ivaylo@tradeo.com>
2014-09-29 15:22:31 +0300

    [Emacs] Set web-mode indent offset to 2

--- .emacs.d/setup-packages.el
+++ .emacs.d/setup-packages.el
@@ -351,7 +351,7 @@
                  ;; Disable whitespace-mode.
                  (whitespace-mode -1)
 
-                 (setq web-mode-markup-indent-offset 4)
+                 (setq web-mode-markup-indent-offset 2)
                  (setq web-mode-css-indent-offset 2)
                  (setq web-mode-code-indent-offset 4)
                  (setq web-mode-disable-autocompletion t)
