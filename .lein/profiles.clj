69c3954b2c577f605900fb0400914601151ac835
ivo- <ivailo.nikolaev.hristov@gmail.com>
2016-01-22 17:02:04 +0200

    Update lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,3 @@
-{:user {:plugins [[lein-try "0.3.2"]
-                  [lein-exec "0.3.1"]
-                  [lein-pprint "1.1.1"]
-                  [lein-ancient "0.4.4"]
-                  [lein-bikeshed "0.1.3"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.7"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
